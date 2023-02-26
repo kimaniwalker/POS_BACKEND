9c679cabec18d83e9b2ffb23caceb24621ea40c7
Kimani Walker <kimani.walker@shipt.com>
2023-02-25 22:46:13 -0600

    [feat]-add header to success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Header from '@/components/Header';
 
 export default function Profile() {
   const { user, error, isLoading }:any = useUser();
@@ -8,12 +9,19 @@ export default function Profile() {
   if (error) return <div>{error.message}</div>;
 
   return (
-    user && (
+    <>
+    
+    <Header />
+    
+   { user && (
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
-    )
+    
+    )  }
+    </>
+    
   );
 }
