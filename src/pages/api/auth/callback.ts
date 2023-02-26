b3e2b343267adcd5a73cdcadd8d65e086dcb51b4
Kimani Walker <kimani.walker@shipt.com>
2023-02-25 21:56:56 -0600

    [feat]-add new route

---
+++ src/pages/api/auth/callback.ts
@@ -0,0 +1,12 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default function getAllMenuItems(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+  
+    res.redirect('/success')
+
+}
